228b778cd83077da3fab8e96ec56c5042fc751b6
sungbosim-33 <sungbosim5@suwon.ac.kr>
2026-04-30 12:23:08 +0900

    과제3 코딩과 테스트 완료 3가지 파일로저장 기능 구

--- Form1.cs
+++ Form1.cs
@@ -1,113 +1,112 @@
-using System.Drawing.Printing;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
+using System.Drawing.Imaging; // 이미지 포맷 지정을 위해 필수
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
+using System.IO; // 파일 경로 및 확장자 처리를 위해 추가
 
 namespace SimplePaint
 {
     public partial class Form1 : Form
     {
-        enum ToolType{ Line, Rectangle, Circle }  // 사용할도형타입
-        private Bitmap canvasBitmap;          // 실제그림이저장되는비트맵
-        private Graphics canvasGraphics;      // 비트맵위에그리기위한객체
-        private bool isDrawing= false;       // 현재드래그중인지여부
-        private Point startPoint;             // 드래그시작점
-        private Point endPoint;               // 드래그끝점
-        private ToolType currentTool= ToolType.Line;  // 현재선택된도형
-        private Color currentColor= Color.Black;      //현재색상
-        private int currentLineWidth= 2;              // 현재선두께
+        enum ToolType { Line, Rectangle, Circle }
+        private Bitmap canvasBitmap;
+        private Graphics canvasGraphics;
+        private bool isDrawing = false;
+        private Point startPoint;
+        private Point endPoint;
+        private ToolType currentTool = ToolType.Line;
+        private Color currentColor = Color.Black;
+        private int currentLineWidth = 2;
 
         public Form1()
         {
             InitializeComponent();
 
-            // 캔버스초기화
-            canvasBitmap= new Bitmap(picCanvas.Width, picCanvas.Height);
-            canvasGraphics= Graphics.FromImage(canvasBitmap);
-            canvasGraphics.Clear(Color.White);   // 캔버스를흰색으로초기화
-            
-            picCanvas.Image= canvasBitmap;   // 그린그림을화면(PictureBox)에표시
+            // 캔버스 초기화
+            canvasBitmap = new Bitmap(picCanvas.Width, picCanvas.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.Clear(Color.White);
+            picCanvas.Image = canvasBitmap;
 
-            // 마우스이벤트연결
-            picCanvas.MouseDown+= PicCanvas_MouseDown;
-            picCanvas.MouseMove+= PicCanvas_MouseMove;
-            picCanvas.MouseUp+= PicCanvas_MouseUp;
-            // picCanvas가다시그려질때PicCanvas_Paint함수를실행하도록연결
-            picCanvas.Paint+= PicCanvas_Paint;
-            
-            // 도형선택버튼이벤트연결
-            btnLine.Click+= btnLine_Click;
-            btnRectangle.Click+= btnRectangle_Click;
-            btnCircle.Click+= btnCircle_Click;
+            // 이벤트 연결
+            picCanvas.MouseDown += PicCanvas_MouseDown;
+            picCanvas.MouseMove += PicCanvas_MouseMove;
+            picCanvas.MouseUp += PicCanvas_MouseUp;
+            picCanvas.Paint += PicCanvas_Paint;
 
-            // 색상콤보박스이벤트연결
-            cmbColor.SelectedIndexChanged+= cmbColor_SelectedIndexChanged;
-            cmbColor.SelectedIndex= 0;  // 기본값: Black
-            
-            // 선두께트랙바이벤트연결
-            trbLineWidth.Minimum= 1;    // 최소값
-            trbLineWidth.Maximum= 10;   // 최대값
-            trbLineWidth.Value= 2;
-            trbLineWidth.ValueChanged+= trbLineWidth_ValueChanged;
+            btnLine.Click += btnLine_Click;
+            btnRectangle.Click += btnRectangle_Click;
+            btnCircle.Click += btnCircle_Click;
+
+            // '저장' 버튼 이벤트 연결 (image_287739.png 요구사항)
+            btnSaveFile.Click += btnSave_Click;
+
+            cmbColor.SelectedIndexChanged += cmbColor_SelectedIndexChanged;
+            cmbColor.SelectedIndex = 0;
+
+            trbLineWidth.Minimum = 1;
+            trbLineWidth.Maximum = 10;
+            trbLineWidth.Value = 2;
+            trbLineWidth.ValueChanged += trbLineWidth_ValueChanged;
         }
-        private void PicCanvas_MouseDown(object sender, MouseEventArgs  e)
+
+        private void PicCanvas_MouseDown(object sender, MouseEventArgs e)
         {
-            isDrawing = true;             // 드래그시작
-            startPoint= e.Location;      // 시작점저장
-         }
-        private void PicCanvas_MouseMove(object sender, MouseEventArgs  e)
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void PicCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!isDrawing) return;       // 그림그리기와상관없는마우스움직임은무시
-            
-            endPoint= e.Location;        // 현재위치갱신
-            
-            // picCanvas를다시그려라(Paint 이벤트를발생시킨다)
-            
-            picCanvas.Invalidate();       // 화면다시그리기(미리보기)
-         }
-        private void PicCanvas_MouseUp(object sender, MouseEventArgs  e)
+            if (!isDrawing) return;
+            endPoint = e.Location;
+            picCanvas.Invalidate();
+        }
+
+        private void PicCanvas_MouseUp(object sender, MouseEventArgs e)
         {
-            if (!isDrawing) return;     // 그림그리기와상관없는마우스움직임은무시
-            
-            isDrawing= false;          // 드래그종료
-            
-            endPoint= e.Location;
-            
-            // 실제비트맵에도형그리기(확정)
-            using (Pen pen= new Pen(currentColor, currentLineWidth))
+            if (!isDrawing) return;
+            isDrawing = false;
+            endPoint = e.Location;
+
+            using (Pen pen = new Pen(currentColor, currentLineWidth))
             {
                 DrawShape(canvasGraphics, pen, startPoint, endPoint);
             }
-            picCanvas.Invalidate();     // 다시그려서결과반영, Paint 이벤트발생
+            picCanvas.Invalidate();
         }
-        private void PicCanvas_Paint(object sender, PaintEventArgs  e)
+
+        private void PicCanvas_Paint(object sender, PaintEventArgs e)
         {
             if (!isDrawing) return;
-            
-            // 점선펜(미리보기용)
-            using (Pen previewPen= new Pen(currentColor, currentLineWidth))
+
+            using (Pen previewPen = new Pen(currentColor, currentLineWidth))
             {
-                previewPen.DashStyle= DashStyle.Dash;
+                previewPen.DashStyle = DashStyle.Dash;
                 DrawShape(e.Graphics, previewPen, startPoint, endPoint);
             }
         }
-        private void DrawShape(Graphics g, Pen pen, Point p1, Point p2) 
+
+        private void DrawShape(Graphics g, Pen pen, Point p1, Point p2)
         {
+            g.SmoothingMode = SmoothingMode.AntiAlias; // 선을 부드럽게 처리
             Rectangle rect = GetRectangle(p1, p2);
             switch (currentTool)
-            { 
-                case ToolType.Line: 
+            {
+                case ToolType.Line:
                     g.DrawLine(pen, p1, p2);
-                    break; case ToolType.Rectangle:
-                    g.DrawRectangle(pen, rect); 
-                    break; case ToolType.Circle: 
-                    g.DrawEllipse(pen, rect); break;
-            } 
+                    break;
+                case ToolType.Rectangle:
+                    g.DrawRectangle(pen, rect);
+                    break;
+                case ToolType.Circle:
+                    g.DrawEllipse(pen, rect);
+                    break;
+            }
         }
-        private Rectangle GetRectangle(Point p1, Point p2) 
+
+        private Rectangle GetRectangle(Point p1, Point p2)
         {
             return new Rectangle(
                 Math.Min(p1.X, p2.X),
@@ -115,42 +114,57 @@ namespace SimplePaint
                 Math.Abs(p1.X - p2.X),
                 Math.Abs(p1.Y - p2.Y));
         }
-        private void btnLine_Click(object sender, EventArgs  e) 
-        { 
-            currentTool = ToolType.Line;
-        }
-        private void btnRectangle_Click(object sender, EventArgs e) 
+
+        // --- 파일 저장 기능 (SaveFileDialog 사용) ---
+        private void btnSave_Click(object sender, EventArgs e)
         {
-            currentTool = ToolType.Rectangle; 
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Title = "이미지 저장";
+                sfd.OverwritePrompt = true;
+                // .png, .jpg, .bmp 3가지 포맷 필터 설정
+                sfd.Filter = "PNG 파일 (*.png)|*.png|JPEG 파일 (*.jpg)|*.jpg|BMP 파일 (*.bmp)|*.bmp";
+                sfd.DefaultExt = "png";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string ext = Path.GetExtension(sfd.FileName).ToLower();
+                        ImageFormat format = ImageFormat.Png;
+
+                        if (ext == ".jpg" || ext == ".jpeg") format = ImageFormat.Jpeg;
+                        else if (ext == ".bmp") format = ImageFormat.Bmp;
+
+                        canvasBitmap.Save(sfd.FileName, format);
+                        MessageBox.Show("저장이 완료되었습니다.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("오류 발생: " + ex.Message);
+                    }
+                }
+            }
         }
-        private void btnCircle_Click(object sender, EventArgs  e) 
-        {
-            currentTool = ToolType.Circle; 
-        }
-        private void cmbColor_SelectedIndexChanged(object sender, EventArgs  e)
+
+        private void btnLine_Click(object sender, EventArgs e) => currentTool = ToolType.Line;
+        private void btnRectangle_Click(object sender, EventArgs e) => currentTool = ToolType.Rectangle;
+        private void btnCircle_Click(object sender, EventArgs e) => currentTool = ToolType.Circle;
+
+        private void cmbColor_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch (cmbColor.SelectedIndex)
             {
-                case 0: // Black 검정
-                    currentColor= Color.Black;
-                    break;
-                case 1: // Red 빨강
-                    currentColor= Color.Red;
-                    break;
-                case 2: // Blue 파랑
-                    currentColor= Color.Blue;
-                    break;
-                case 3: // Green 녹색
-                    currentColor= Color.Green;
-                    break;
-                default:
-                    currentColor= Color.Black;
-                    break;
+                case 0: currentColor = Color.Black; break;
+                case 1: currentColor = Color.Red; break;
+                case 2: currentColor = Color.Blue; break;
+                case 3: currentColor = Color.Green; break;
             }
         }
-        private void trbLineWidth_ValueChanged(object sender, EventArgs e) 
+
+        private void trbLineWidth_ValueChanged(object sender, EventArgs e)
         {
-            currentLineWidth = trbLineWidth.Value; 
+            currentLineWidth = trbLineWidth.Value;
         }
     }
 }
